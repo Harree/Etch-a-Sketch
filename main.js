33a5eff884d40332b8a1e9ce43ee16a866af6f42
Harree <educationalmadz+GitHub@gmail.com>
2022-02-23 21:16:35 +0000

    center drawing canvas, add reset button function.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let resetTrigger = document.getElementById('reset');
+
 function populateBoard(size){
 let board = document.querySelector('.board');
 let squares = board.querySelectorAll('div');
@@ -11,17 +13,24 @@ let amount = size * size;
 for(let i = 0; i < amount; i++) {
     let square = document.createElement('div');
     square.addEventListener('mouseover', function(){
-        this.style.backgroundColor = "black";
+        this.style.backgroundColor = "black"; //allows the user to draw on the generated canvas.
     })
     square.style.backgroundColor = "yellow";
     board.insertAdjacentElement('beforeend', square);
 }
 };
 
-populateBoard(16);
+populateBoard(16); //calls the function with a default size of 16 squares
 
+//this allows the user to decide on the size of the canvas while imposing a limit to prevent browser crash. 
 function changeSize(input) {
     if(input>=2 && input <=100){
     populateBoard(input); 
 } else {alert("error, please insert a number between 2 and 100")};
 }
+
+
+
+resetTrigger.addEventListener('click', () => {
+    window.location.reload();
+});
